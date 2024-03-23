fdf1e65674919a2022bd77a8f01b9ceea111d0b6
jayyyyyy1 <jayramcharan2104@gmail.com>
2024-03-23 22:12:22 +0100

    wat invalid input gefixed

--- Project/Presentation/Adding.cs
+++ Project/Presentation/Adding.cs
@@ -10,8 +10,22 @@ static class Adding
         Console.WriteLine("Add food to the menu:");
         Console.WriteLine("Please enter the name of the food item");
         string name = Console.ReadLine();
-        Console.WriteLine("Please enter the price of the food item");
-        decimal price = Convert.ToDecimal(Console.ReadLine());
+
+        decimal price;
+        while (true)
+        {
+            Console.WriteLine("Please enter the price of the food item");
+            string priceInput = Console.ReadLine();
+            if (decimal.TryParse(priceInput, out price))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid price.");
+            }
+        }
+        
         FoodModel foodItem = new FoodModel(name, price);
         foodLogic.UpdateList(foodItem);
 
@@ -24,14 +38,42 @@ static class Adding
         string name = Console.ReadLine();
         Console.WriteLine("Please enter the genre of the movie");
         string genre = Console.ReadLine();
-        Console.WriteLine("Please enter the year of the movie");
-        int year = Convert.ToInt32(Console.ReadLine());
+
+        int year;
+        while (true)
+        {
+            Console.WriteLine("Please enter the year of the movie");
+            string yearInput = Console.ReadLine();
+            if (int.TryParse(yearInput, out year))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid year.");
+            }
+        }
+
         Console.WriteLine("Please enter the description of the movie");
         string description = Console.ReadLine();
         Console.WriteLine("Please enter the director of the movie");
         string director = Console.ReadLine();
-        Console.WriteLine("Please enter the duration of the movie (minutes)");
-        int duration = Convert.ToInt32(Console.ReadLine());
+
+        int duration;
+        while (true)
+        {
+            Console.WriteLine("Please enter the duration of the movie (minutes)");
+            string durationInput = Console.ReadLine();
+            if (int.TryParse(durationInput, out duration))
+            {
+                break; 
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid duration.");
+            }
+        }
+
         MovieModel movies = new MovieModel(name, genre, year, description, director, duration);
         movieLogic.UpdateList(movies);
 
