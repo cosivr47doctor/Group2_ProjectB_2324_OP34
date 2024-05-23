e6e70a119df2ce3d0fd5ab158f403951a4744ff2
jayyyyyy1 <jayramcharan2104@gmail.com>
2024-05-23 14:09:58 +0200

    menu admin shi fixed costa slapen jij

--- Project/Presentation/Menus/UserMenu.cs
+++ Project/Presentation/Menus/UserMenu.cs
@@ -3,30 +3,28 @@ static class UserMenu
 {
     static public void Start(int accId=0, bool isAdmin=false)
     {
-
         List<string> options = new(){
-            "Switch back to admin menu",
             "Logout",
             "Make a reservation",
             "Order some food",
             "See all available movies",
             "See the movies schedule",
             "Search a movie",
-            "See reservation history"
-
+            "See reservation history",
         };
+
+        if (isAdmin)
+        {
+            options.Add("Enter admin menu");
+        }
 
         int selectedOption = DisplayUtil.MenuDisplay(options);
         switch (selectedOption)
         {
             case 0:
-                if (isAdmin) AdminMenu.Start(accId);
-                else goto default;
-                break;
-            case 1:
                 MainMenu.Start();
                 break;
-            case 2:
+            case 1:
                 SeeJsons.PrintMoviesJson(@"DataSources/movies.json");
                 Console.WriteLine("");
                 //Reservation.ReserveMovie(accId);
@@ -37,7 +35,7 @@ static class UserMenu
                 Console.ReadLine();
                 Start(accId, isAdmin);
                 break;
-            case 3:
+            case 2:
                 SeeJsons.PrintFoodJson(@"DataSources/food.json");
                 //Reservation.ReserveFood(accId);
 
@@ -47,14 +45,14 @@ static class UserMenu
 
                 Start(accId, isAdmin);
                 break;
-            case 4:
+            case 3:
                 SeeJsons.PrintMoviesJson(@"DataSources/movies.json");
                 Console.WriteLine("");
                 Console.WriteLine("Press enter to go back.");
                 Console.ReadLine();
                 Start(accId, isAdmin);
                 break;
-            case 5:
+            case 4:
                 MovieSchedulingLogic objMovieSchedulingLogic = new();
                 List<string> scheduleOptions = new(){
                 "See entire schedule",
@@ -94,14 +92,15 @@ static class UserMenu
                 Console.ReadLine();
                 Start(accId, isAdmin);
                 break;
-            case 6:
+            case 5:
                 Search.searchMovie();
                 Console.WriteLine("");
                 Console.WriteLine("Press enter to go back.");
                 Console.ReadLine();
                 Start(accId, isAdmin);
                 break;
-            case 7:
+            case 6:
+                Console.ResetColor();
                 Console.WriteLine("Reservation history function");
                 Console.WriteLine("");
                 ResvDetails.ResvHistory(accId);
@@ -114,9 +113,20 @@ static class UserMenu
                 Console.WriteLine("Invalid input");
                 Start(accId, isAdmin);
                 break;
+            case 7:
+                if (isAdmin) 
+                {
+                    Console.ResetColor();
+                    AdminMenu.Start(accId);
+                }
+                else 
+                {
+                    Console.ResetColor();
+                    Console.WriteLine("I don't think you have the facilities for that big man");
+                    Thread.Sleep(1000);
+                    Start(accId, isAdmin);
+                }
+                break;
         }
-
-
-
     }
 }
